84d760370929810d1e64f8acd5ad085ecf00b575
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-05-10 22:03:21 +0530

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,24 +1,25 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import {connectdb} from './db/db.config.js'
-import { urlRouter } from './route/url.route.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectdb } from "./db/db.config.js";
+import { urlRouter } from "./route/url.route.js";
 
 //middleware configuration
 dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
 
 //database connection
 connectdb();
 
-
 //api endpoints
-app.use("/", urlRouter)
-
-
+app.use("/", urlRouter);
 
 //create a http server
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server is running on port ${process.env.PORT}\n${process.env.BACKEND_DOMAIN}`)
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server is running on port ${process.env.PORT}\n${process.env.BACKEND_DOMAIN}`
+  );
 });
