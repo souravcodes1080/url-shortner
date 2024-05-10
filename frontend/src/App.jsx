9ec8310365b0222a20e927854f8d6f64daf720bb
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-05-10 21:01:17 +0530

    responsive css boilerplate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Homepage from "./pages/Homepage/Homepage";
+function App() {
+  return (
+    <>
+      <div className="app-container">
+        <Homepage />
+      </div>
+    </>
+  );
+}
+
+export default App;
