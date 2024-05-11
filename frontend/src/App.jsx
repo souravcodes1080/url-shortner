447d2e0695f872cfd3742716e7cbdec8a1a3b228
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-05-11 10:16:19 +0530

    added toastify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Slide, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Homepage from "./pages/Homepage/Homepage";
 function App() {
   return (
     <>
       <Homepage />
+      <ToastContainer
+        position="bottom-center"
+        autoClose={3000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+        theme="light"
+        transition:Slide
+      />
     </>
   );
 }
--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -2,31 +2,32 @@ import React, { useEffect, useState } from "react";
 import "./main.css";
 import cross_icon from "../../assets/cross_icon.png";
 import url_icon from "../../assets/url_icon.png";
+import hero from "../../assets/hero.png";
 import axios from "axios";
 import { BACKEND_DOMAIN } from "../../config";
+import { Slide, toast } from "react-toastify";
+
 function Main() {
   const [longUrl, setLongUrl] = useState("");
   const [shortUrl, setShortUrl] = useState("");
   const [showResult, setShowResult] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  useEffect(()=>{
-    if(localStorage.getItem("shortUrl")){
-      setShowResult(true)
+  useEffect(() => {
+    if (localStorage.getItem("shortUrl")) {
+      setShowResult(true);
     }
-  }, [])
+  }, []);
   const onChangeHandler = (e) => {
     setLongUrl(e.target.value);
   };
   const onSubmitHandler = async (e) => {
     e.preventDefault();
-    localStorage.setItem("longUrl", longUrl);
     setShowResult(true);
     setLoading(true);
     const response = await axios.post(`${BACKEND_DOMAIN}/api/shorten`, {
       longUrl: longUrl,
     });
-    console.log(response.data);
     if (response.data.success) {
       setLoading(false);
       setShortUrl(response.data.shortUrl);
@@ -34,24 +35,47 @@ function Main() {
     } else {
       setLoading(false);
       setShowResult(false);
-      alert(response.data.message);
+      toast.error(response.data.message, {
+        position: "bottom-center",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Slide,
+      });
     }
   };
   const copyToClipboard = () => {
     navigator.clipboard.writeText(shortUrl).then(() => {
-      alert("copied to clipboard!!");
+      toast.success("Copied to clipboard!", {
+        position: "bottom-center",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Slide,
+      });
     });
   };
 
   return (
     <>
       <main>
+        <div className="main-hero-img">
+          <img src={hero} alt="hero_image" loading="lazy" />
+        </div>
         <h1>Free URL Shortener</h1>
         <p>Create free short urls within seconds powered by our own api.</p>
         <form onSubmit={onSubmitHandler}>
           <input
             type="text"
-            value={longUrl || localStorage.getItem("longUrl")}
+            value={longUrl}
             onChange={onChangeHandler}
             placeholder="Enter link here"
           />
@@ -67,9 +91,14 @@ function Main() {
                   Copy
                 </button>
                 <a href={shortUrl} target="_blank">
-                  {shortUrl || localStorage.getItem("shortUrl")} <img src={url_icon} alt="url" width={"18px"} />
+                  {shortUrl || localStorage.getItem("shortUrl")}{" "}
+                  <img src={url_icon} alt="url" width={"18px"} />
                 </a>
-                <span onClick={() => {setShowResult(false); localStorage.setItem("longUrl", "")}}>
+                <span
+                  onClick={() => {
+                    setShowResult(false);
+                  }}
+                >
                   <img src={cross_icon} alt="cross" width={"16px"} />
                 </span>
               </div>
@@ -78,7 +107,10 @@ function Main() {
         ) : (
           <></>
         )}
-        <p className="policy">By clicking Shorten URL, you agree to our Terms of Use, Privacy Policy and Cookie Policy</p>
+        <p className="policy">
+          By clicking Shorten URL, you agree to our <span>Terms of Use</span>,
+          Privacy Policy and <span>Cookie Policy</span>
+        </p>
       </main>
     </>
   );
